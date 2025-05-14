3406306a55f97fe7a93c94511110be2c2caca9f0
Milos Vasic <i@mvasic.ru>
2025-05-14 13:18:30 +0300

    Upstreams.

---
+++ Upstreams/GitHub.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export UPSTREAMABLE_REPOSITORY="git@github.com:ATMOSphere1234321/rkbin.git"
---
+++ Upstreams/GitLab.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export UPSTREAMABLE_REPOSITORY="git@gitlab.com:atmosphere1234321/rkbin.git"
